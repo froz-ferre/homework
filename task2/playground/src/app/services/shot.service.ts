3dd8fe988a1d89e5811dfe66fb040daa4dc06e18
froz-ferre <alexej.ferre@gmail.com>
2018-05-02 17:11:57 +0300

    HttpClient

--- task2/playground/src/app/services/shot.service.ts
+++ task2/playground/src/app/services/shot.service.ts
@@ -1,20 +1,27 @@
+import { Shot } from './../model/shot';
+import { data } from './../model/jshots';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { Shot } from '../model/shot';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ShotService {
 
+  shots;
+
   constructor(private http: HttpClient) {
-    http.get('../../assets/shots.json').subscribe(r => console.log(r.toString()));
+    // http.get('../../assets/shots.json').subscribe(r => console.log(r.toString()));
    }
 
-  getShots(): Observable<Shot[]> {
-    return this.http.get('../../assets/shots.json')
-    .subscribe((res: Response) => res.json());
+  getShots(): Observable<any> {
+    return this.http.get('../../assets/shots.json');
+    // .map((res: Response) => res.json())
+    // .subscribe(data => {
+    //   this.shots.push(data);
+    // });
+    // .map((res: Response) => res.json());
   }
 }
 
-// CYKAAAAAAAAA!!!!
+// https://www.develodesign.co.uk/news/angular/using-mouse-events-in-angular-4-5/
