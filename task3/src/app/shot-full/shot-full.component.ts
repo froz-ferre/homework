8574dd48710fb51c286fdd8f136c82e2082ae45c
froz-ferre <alexej.ferre@gmail.com>
2018-05-13 15:28:59 +0300

    Download shot img on shot list and AJAX downloader in shot full preview

--- task3/src/app/shot-full/shot-full.component.ts
+++ task3/src/app/shot-full/shot-full.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 import { Shot } from '../model/shot';
 import { ShotService } from '../services/shot.service';
 
@@ -12,14 +13,28 @@ export class ShotFullComponent implements OnInit {
 
   shotId: number;
   shot: Shot;
+  @ViewChild('downloadHelper') downloadHelper;
 
   constructor(private activatedRoute: ActivatedRoute,
-              private shotService:    ShotService) {
+              private shotService:    ShotService,
+              private http:           HttpClient,
+              private el:             ElementRef) {
     activatedRoute.params.subscribe(param => this.shotId = +param['shotID']);
   }
 
   ngOnInit() {
     this.shotService.getShot(this.shotId).subscribe(shot => this.shot = shot);
   }
+
+  downloadFile() {
+    this.http.get(`${this.shot.thumbnail}`, {responseType: 'blob'}).subscribe(res => {
+      const link = this.downloadHelper.nativeElement;
+      const url = window.URL.createObjectURL(res);
+      link.href = url;
+      link.download = `${this.shot.title}.jpg`;
+      link.click();
+      window.URL.revokeObjectURL(url);
+    });
+}
 
 }
